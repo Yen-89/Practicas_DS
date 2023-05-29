cd1750a1d9a6cbfe713cf3def59c219f8e269acf
Yen <yenifer.amayaos@gmail.com>
2023-05-29 15:33:36 -0500

    N1

--- PyCharm/Notes_1.py
+++ PyCharm/Notes_1.py
@@ -7,5 +7,6 @@ print("Hello World")
 # print(input("Dime tu nombre: "))
 # print(input("Dime tu apellido: "))
 
+## Es valido de estas dos maneras
 print("Tu nombre es " + input("Dime tu nombre: ") + " " + input("Dime tu apellido: "))
 print("Tu nombre es " + (input("Dime tu nombre: ")) + " " + (input("Dime tu apellido: ")))
