603ad3644f7ef6a25cdad66ed527dbda7ed07b91
Yen <yenifer.amayaos@gmail.com>
2023-05-29 15:33:12 -0500

    Notas _1

---
+++ PyCharm/Notes_1.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+# Function ''Input'' para que el usuario sea el que ingrese los datos
+
+## print(input("your name"))  # IMPRIMIR lo que INGRESEN al pedir el NOMBRE
+
+# print(input("Dime tu nombre: "))
+# print(input("Dime tu apellido: "))
+
+print("Tu nombre es " + input("Dime tu nombre: ") + " " + input("Dime tu apellido: "))
+print("Tu nombre es " + (input("Dime tu nombre: ")) + " " + (input("Dime tu apellido: ")))
